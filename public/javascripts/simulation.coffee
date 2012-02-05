75713e756384c409f86d6424b43e30c7edb9a4ca
Jordi Polo <mumismo@gmail.com>
2012-02-05 13:56:40 +0900

    initial commit

---
+++ public/javascripts/simulation.coffee
@@ -0,0 +1,215 @@
+###########################################
+# Globals 
+###########################################
+#Gravitation force constant
+G = 6.6742e-11
+#Initial drawing scale
+DRAW_SCALE = 9e-10
+#hours the universe advance every step
+hours_per_step = 30
+# actual variable used in physics (we use seconds)
+dt = 3600 * hours_per_step
+#refresh rate in ms
+refresh_rate = 20
+
+planets_data = [
+  "Sun",0,695000000,1.989E+030,0, "yellow"
+  "Mercury",57900000000,2440000,3.33E+023,47900, "gray"
+  "Venus",108000000000,6050000,4.869E+024,35000, "red"
+  "Earth",150000000000,6378140,5.976E+024,29800, "blue"
+  "Mars",227940000000,3397200,6.421E+023,24100, "red"
+  "Jupiter",778330000000,71492000,1.9E+027,13100, "brown"
+  "Saturn",1429400000000,60268000,5.688E+026,9640, "gray"
+  "Uranus",2870990000000,25559000,8.686E+025,6810, "gray"
+  "Neptune",4504300000000,24746000,1.024E+026,5430, "yellow"
+  "Pluto",5913520000000,1137000,1.27E+022,4740, "black"
+]
+
+
+
+
+###########################################
+# A Point in 3D space, very badly programmed but 
+# it seems I can not define -, +, *, / in CS 
+###########################################
+class Point3d
+  constructor: (@x, @y, @z) ->
+
+  print: () ->
+    console.log " x: #{@x}, y: #{@y}, z: #{@z} "
+ 
+  minus: (p) ->
+    new Point3d(@x - p.x, @y - p.y, @z - p.z)
+
+  plus: (p) ->
+    new Point3d(@x + p.x, @y + p.y, @z + p.z)
+
+  divided: (p) ->
+    new Point3d(@x / p, @y / p, @z / p)
+
+  multiply: (p) ->
+    new Point3d(@x * p, @y * p, @z * p)
+
+  distance: () ->
+    @x*@x + @y*@y + @z*@z
+
+
+###########################################
+# A planet 
+###########################################
+class Planet
+  constructor: (@name, @radius, @mass, @pos, @vel, @color ) ->
+
+  draw: () ->
+    scaled_center_x = (canvas_element.width / 2 ) / scale
+    scaled_center_y = (canvas_element.height / 2 ) / scale
+    center_x = (1/scale) *( 450 + @pos.x * DRAW_SCALE )
+    center_y = (1/scale) *( 300 + @pos.z * DRAW_SCALE)
+    center_x = scaled_center_x + @pos.x * DRAW_SCALE
+    center_y = scaled_center_y + @pos.z * DRAW_SCALE
+    radius = @radius * DRAW_SCALE * 40
+    if @name isnt "Sun"
+      radius *= 10
+     
+    canvas.beginPath()
+    canvas.arc(center_x, center_y, radius, 0, 2 * Math.PI, false)
+    canvas.fillStyle = @color
+    canvas.fill()
+    canvas.lineWidth = 1
+    canvas.strokeStyle = "black"
+    canvas.stroke()
+
+    if @name isnt "Sun"
+      below = center_y + radius + 5
+      canvas.fillText(@name, center_x, below)
+
+  print: () ->
+    #console.log @name, @pos
+
+
+###########################################
+# The solar system
+###########################################
+class SolarSystem
+  constructor: () ->
+
+  load: () ->
+    @planets = []
+    for planet_data, i in planets_data by 6
+      name = planets_data[i]
+      pos = new Point3d(planets_data[i + 1], 0,0)
+      vel = new Point3d(0, 0, planets_data[i + 4])
+      radius = planets_data[i+2]
+      mass = planets_data[i+3]
+      color = planets_data[i+5]
+      planet = new Planet(name, radius, mass, pos, vel, color)
+      @planets.push planet
+      planet.print()
+    @sun = @planets[0]
+
+  step: (dt) ->
+    for planet in @planets
+      if planet isnt @sun
+        this.update_planet(planet, dt)
+      planet.draw()
+
+  update_planet: (planet, dt) ->
+    radius = planet.pos
+    distance = radius.distance()
+    acc = (-G * @sun.mass) / distance
+    diff_vel = radius.divided( Math.sqrt(distance))
+    diff_vel = diff_vel.multiply( acc * dt)
+    planet.vel  = planet.vel.plus( diff_vel )
+    planet.pos = planet.pos.plus( planet.vel.multiply(dt) )
+
+  print: () ->
+    for planet in @planets
+      planet.print()
+ 
+
+
+###########################################
+# Control and display of the simulation
+###########################################
+class Simulation
+  constructor: () ->
+    @current_time = 0
+    @solar_system = new SolarSystem
+    @simulation_running = false
+    
+  start: () =>
+    return if @simulation_running
+    @current_time = 0
+    @simulation_running = true
+    @solar_system.load()
+    this.step()
+
+  stop: () =>
+    @simulation_running = false
+
+  step: () =>
+    this.clear_canvas()
+
+    @current_time += (dt /3600)
+    @solar_system.step(dt)
+    this.display_info()
+    if (@simulation_running)
+      setTimeout(this.step, refresh_rate)
+
+  display_info: () =>
+    time_in_days = Math.floor(@current_time / 24)
+    days = time_in_days % 365
+    years = Math.floor( time_in_days / 365.25 )
+    canvas.save()
+    canvas.setTransform(1,0,0,1,0,0)
+    canvas.fillText("#{years} years, #{days} days ", 10, 20)
+    canvas.restore()
+
+  #this can go to a canvas class
+  clear_canvas: =>
+    canvas.save()
+    canvas.setTransform(1,0,0,1,0,0)
+    canvas.clearRect(0, 0, canvas_element.width, canvas_element.height)
+    canvas.restore()
+
+
+canvas_element = document.getElementById("main")
+canvas = canvas_element.getContext "2d"
+
+
+simulation = new Simulation
+document.getElementById("start_button").addEventListener "click", simulation.start
+document.getElementById("stop_button").addEventListener "click", simulation.stop
+
+simulation.start()
+
+
+
+#####################################
+# Zoom for the canvas. 
+# scale variable is used by the planets to render correctly
+# TODO: this is kind of thrown away here
+#####################################
+
+scale = 1
+originx = 0
+originy = 0
+
+canvas_element.onmousewheel = (event) ->
+  mousex = event.clientX - canvas.offsetLeft
+  mousey = event.clientY - canvas.offsetTop
+  wheel = event.wheelDelta/120
+
+  zoom = 1 + wheel/8
+
+  canvas.translate( originx, originy)
+
+  canvas.scale(zoom,zoom)
+  canvas.translate(
+        -( mousex / scale + originx - mousex / ( scale * zoom ) ),
+        -( mousey / scale + originy - mousey / ( scale * zoom ) )
+  )
+
+  originx = ( mousex / scale + originx - mousex / ( scale * zoom ) )
+  originy = ( mousey / scale + originy - mousey / ( scale * zoom ) )
+  scale *= zoom
---
+++ public/javascripts/simulation.js
@@ -0,0 +1,239 @@
+(function() {
+  var DRAW_SCALE, G, Planet, Point3d, Simulation, SolarSystem, canvas, canvas_element, dt, hours_per_step, originx, originy, planets_data, refresh_rate, scale, simulation,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  G = 6.6742e-11;
+
+  DRAW_SCALE = 9e-10;
+
+  hours_per_step = 30;
+
+  dt = 3600 * hours_per_step;
+
+  refresh_rate = 20;
+
+  planets_data = ["Sun", 0, 695000000, 1.989E+030, 0, "yellow", "Mercury", 57900000000, 2440000, 3.33E+023, 47900, "gray", "Venus", 108000000000, 6050000, 4.869E+024, 35000, "red", "Earth", 150000000000, 6378140, 5.976E+024, 29800, "blue", "Mars", 227940000000, 3397200, 6.421E+023, 24100, "red", "Jupiter", 778330000000, 71492000, 1.9E+027, 13100, "brown", "Saturn", 1429400000000, 60268000, 5.688E+026, 9640, "gray", "Uranus", 2870990000000, 25559000, 8.686E+025, 6810, "gray", "Neptune", 4504300000000, 24746000, 1.024E+026, 5430, "yellow", "Pluto", 5913520000000, 1137000, 1.27E+022, 4740, "black"];
+
+  Point3d = (function() {
+
+    function Point3d(x, y, z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+
+    Point3d.prototype.print = function() {
+      return console.log(" x: " + this.x + ", y: " + this.y + ", z: " + this.z + " ");
+    };
+
+    Point3d.prototype.minus = function(p) {
+      return new Point3d(this.x - p.x, this.y - p.y, this.z - p.z);
+    };
+
+    Point3d.prototype.plus = function(p) {
+      return new Point3d(this.x + p.x, this.y + p.y, this.z + p.z);
+    };
+
+    Point3d.prototype.divided = function(p) {
+      return new Point3d(this.x / p, this.y / p, this.z / p);
+    };
+
+    Point3d.prototype.multiply = function(p) {
+      return new Point3d(this.x * p, this.y * p, this.z * p);
+    };
+
+    Point3d.prototype.distance = function() {
+      return this.x * this.x + this.y * this.y + this.z * this.z;
+    };
+
+    return Point3d;
+
+  })();
+
+  Planet = (function() {
+
+    function Planet(name, radius, mass, pos, vel, color) {
+      this.name = name;
+      this.radius = radius;
+      this.mass = mass;
+      this.pos = pos;
+      this.vel = vel;
+      this.color = color;
+    }
+
+    Planet.prototype.draw = function() {
+      var below, center_x, center_y, radius, scaled_center_x, scaled_center_y;
+      scaled_center_x = (canvas_element.width / 2) / scale;
+      scaled_center_y = (canvas_element.height / 2) / scale;
+      center_x = (1 / scale) * (450 + this.pos.x * DRAW_SCALE);
+      center_y = (1 / scale) * (300 + this.pos.z * DRAW_SCALE);
+      center_x = scaled_center_x + this.pos.x * DRAW_SCALE;
+      center_y = scaled_center_y + this.pos.z * DRAW_SCALE;
+      radius = this.radius * DRAW_SCALE * 40;
+      if (this.name !== "Sun") radius *= 10;
+      canvas.beginPath();
+      canvas.arc(center_x, center_y, radius, 0, 2 * Math.PI, false);
+      canvas.fillStyle = this.color;
+      canvas.fill();
+      canvas.lineWidth = 1;
+      canvas.strokeStyle = "black";
+      canvas.stroke();
+      if (this.name !== "Sun") {
+        below = center_y + radius + 5;
+        return canvas.fillText(this.name, center_x, below);
+      }
+    };
+
+    Planet.prototype.print = function() {};
+
+    return Planet;
+
+  })();
+
+  SolarSystem = (function() {
+
+    function SolarSystem() {}
+
+    SolarSystem.prototype.load = function() {
+      var color, i, mass, name, planet, planet_data, pos, radius, vel, _len, _step;
+      this.planets = [];
+      for (i = 0, _len = planets_data.length, _step = 6; i < _len; i += _step) {
+        planet_data = planets_data[i];
+        name = planets_data[i];
+        pos = new Point3d(planets_data[i + 1], 0, 0);
+        vel = new Point3d(0, 0, planets_data[i + 4]);
+        radius = planets_data[i + 2];
+        mass = planets_data[i + 3];
+        color = planets_data[i + 5];
+        planet = new Planet(name, radius, mass, pos, vel, color);
+        this.planets.push(planet);
+        planet.print();
+      }
+      return this.sun = this.planets[0];
+    };
+
+    SolarSystem.prototype.step = function(dt) {
+      var planet, _i, _len, _ref, _results;
+      _ref = this.planets;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        planet = _ref[_i];
+        if (planet !== this.sun) this.update_planet(planet, dt);
+        _results.push(planet.draw());
+      }
+      return _results;
+    };
+
+    SolarSystem.prototype.update_planet = function(planet, dt) {
+      var acc, diff_vel, distance, radius;
+      radius = planet.pos;
+      distance = radius.distance();
+      acc = (-G * this.sun.mass) / distance;
+      diff_vel = radius.divided(Math.sqrt(distance));
+      diff_vel = diff_vel.multiply(acc * dt);
+      planet.vel = planet.vel.plus(diff_vel);
+      return planet.pos = planet.pos.plus(planet.vel.multiply(dt));
+    };
+
+    SolarSystem.prototype.print = function() {
+      var planet, _i, _len, _ref, _results;
+      _ref = this.planets;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        planet = _ref[_i];
+        _results.push(planet.print());
+      }
+      return _results;
+    };
+
+    return SolarSystem;
+
+  })();
+
+  Simulation = (function() {
+
+    function Simulation() {
+      this.clear_canvas = __bind(this.clear_canvas, this);
+      this.display_info = __bind(this.display_info, this);
+      this.step = __bind(this.step, this);
+      this.stop = __bind(this.stop, this);
+      this.start = __bind(this.start, this);      this.current_time = 0;
+      this.solar_system = new SolarSystem;
+      this.simulation_running = false;
+    }
+
+    Simulation.prototype.start = function() {
+      if (this.simulation_running) return;
+      this.current_time = 0;
+      this.simulation_running = true;
+      this.solar_system.load();
+      return this.step();
+    };
+
+    Simulation.prototype.stop = function() {
+      return this.simulation_running = false;
+    };
+
+    Simulation.prototype.step = function() {
+      this.clear_canvas();
+      this.current_time += dt / 3600;
+      this.solar_system.step(dt);
+      this.display_info();
+      if (this.simulation_running) return setTimeout(this.step, refresh_rate);
+    };
+
+    Simulation.prototype.display_info = function() {
+      var days, time_in_days, years;
+      time_in_days = Math.floor(this.current_time / 24);
+      days = time_in_days % 365;
+      years = Math.floor(time_in_days / 365.25);
+      canvas.save();
+      canvas.setTransform(1, 0, 0, 1, 0, 0);
+      canvas.fillText("" + years + " years, " + days + " days ", 10, 20);
+      return canvas.restore();
+    };
+
+    Simulation.prototype.clear_canvas = function() {
+      canvas.save();
+      canvas.setTransform(1, 0, 0, 1, 0, 0);
+      canvas.clearRect(0, 0, canvas_element.width, canvas_element.height);
+      return canvas.restore();
+    };
+
+    return Simulation;
+
+  })();
+
+  canvas_element = document.getElementById("main");
+
+  canvas = canvas_element.getContext("2d");
+
+  simulation = new Simulation;
+
+  document.getElementById("start_button").addEventListener("click", simulation.start);
+
+  document.getElementById("stop_button").addEventListener("click", simulation.stop);
+
+  simulation.start();
+
+  scale = 1;
+
+  originx = 0;
+
+  originy = 0;
+
+  canvas_element.onmousewheel = function(event) {
+    var mousex, mousey, wheel, zoom;
+    mousex = event.clientX - canvas.offsetLeft;
+    mousey = event.clientY - canvas.offsetTop;
+    wheel = event.wheelDelta / 120;
+    zoom = 1 + wheel / 8;
+    canvas.translate(originx, originy);
+    canvas.scale(zoom, zoom);
+    canvas.translate(-(mousex / scale + originx - mousex / (scale * zoom)), -(mousey / scale + originy - mousey / (scale * zoom)));
+    originx = mousex / scale + originx - mousex / (scale * zoom);
+    originy = mousey / scale + originy - mousey / (scale * zoom);
+    return scale *= zoom;
+  };
+
+}).call(this);
